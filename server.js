dd00d82260451ed5685cb990ab1c84c3c6a48f4e
Rishav Thakker <thakker.rishav@gmail.com>
2017-11-11 07:17:59 +0530

    Cleaned up, packaged with electron

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 // Create an app
-const shell = require('shelljs')
 const server = require('diet')
-const fs = require('fs')
 const exec = require('child_process').exec
-// var Worker = require('webworker-threads').Worker;
 
 app = server()
 
 k2pdfoptPath = 'k2pdfopt'
-process.env.K2PDFOPT = "-ui- -x"
-shell.config.execPath = '/run/media/LinuxHD/NVM_DIR/versions/node/v9.1.0/bin/electron'
-// shell.config.execPath = '/home/rthakker/.nvm/versions/node/v9.1.0/bin/node'
-// shell.config.execPath = 'path/to/node/binary'; // Replace this with the real path
-
-// Store the status of k2pdfopt
-// status = 'idle'
-// Store buffered output
-// bufferedOutput = null
 
 newFiles = []
 
@@ -29,43 +17,48 @@ app.post('/convert', function($) {
     f = data.files[i]
     console.log('Converting ' + f.path)
     newPath = '/tmp/' + f.name + '_converted.pdf'
-    command = k2pdfoptPath  + ' ' + f.path + ' -ui- -x -a- -o ' + newPath
+    command = k2pdfoptPath + ' ' + f.path + ' -ui- -x -a- -o ' + newPath
     f.newPath = newPath
-    // f.runOutput = shell.exec(command)
-    // args = [f.path, '-ui', '-x', '-a-', '-o', newPath]
     spawnedProcess = exec(command)
     f.convertData = {
       bufferedOutput: '',
       status: null
     }
 
-    spawnedProcess.stdout.on('data', function(data) {
-      console.log('stdout: ' + data)
-      f.convertData.bufferedOutput += '' + data;
-    })
+    spawnedProcess.stdout.on('data', function(tempObj) {
+      return function(data) {
+        console.log('stdout: ' + tempObj.name + data)
+        tempObj.convertData.bufferedOutput += '' + data;
+      }
+    }(f))
 
-    spawnedProcess.on('close', function(code) {
-      if (code !== 0) {
-        f.convertData.status = 'failed'
+    spawnedProcess.on('close', function(tempObj) {
+      return function(code) {
+        if (code !== 0) {
+          tempObj.convertData.status = 'failed'
+        } else {
+          tempObj.convertData.status = 'done'
+        }
       }
-      else {
-        f.convertData.status = 'done'
-      }
-    })
+    }(f))
 
     newFiles.push(f)
   }
-  // $.end(JSON.stringify({files: newFiles}))
   $.end('Success')
 })
 
 app.get('/get_status', function($) {
   $.end(JSON.stringify(newFiles))
-  // $.end(newFiles[0].convertData.bufferedOutput)
+})
+
+// Set exit function
+process.on('message', (msg) => {
+  if (msg == 'exit') {
+    process.exit()
+  }
 })
 
 function startServer() {
   app.listen('http://localhost:8000')
 }
-// var MyServerWorker = new Worker(startServer)
 startServer();
