1ad0a3f11541e6a822f0b5b4a10418aec2c92207
Rishav Thakker <thakker.rishav@gmail.com>
2019-12-20 02:36:20 +0530

    Added License

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@ electron-packager . --build-with-native-modules --platform=darwin --arch=x64 \
   --package-manager=yarn --overwrite --out=./dist \
   --icon=./icons/icon.icns \
   --app-category-type=public.app-category.productivity \
-  --osx-sign.identity="Rishav Thakker" \
+  # --osx-sign.identity="Rishav Thakker" \
+
+cd ./dist/Rayk-darwin-x64
+mv ./LICENSE ./LICENSE-Electron
+mv ./version ./version-Electron
+cd -
+cp ./LICENSE ./dist/Rayk-darwin-x64
